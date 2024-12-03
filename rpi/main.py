6f377917c305601bf4fb6851300195de3b82b820
Ashton A <uhdbits@duck.com>
2024-12-02 21:41:41 -0700

    Add git commands to RPi
    Signed-off-by: Ashton A <uhdbits@duck.com>

--- rpi/main.py
+++ rpi/main.py
@@ -6,6 +6,7 @@ import time
 
 
 if __name__ == '__main__':
+    subprocess.call(['git', 'pull'])
     arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     arduino.reset_input_buffer()
     while True:
@@ -13,9 +14,11 @@ if __name__ == '__main__':
             data = json.load(f)
         if arduino.in_waiting > 0:
             lines = arduino.readlines()
-            data['readings'].append({"time": datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"), "level": lines[-1].decode('utf-8').strip()})
+            data['readings'].append({"time": datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"), "level": str(int(lines[-1].decode('utf-8').strip()) / 10000)})
 
         with open('data.json', 'w') as f:
             json.dump(data, f)
-        time.sleep(2.0)
+        subprocess.call(['git', 'add', 'data.json'])
+        subprocess.call(['git', 'commit', '-m', 'Periodic Update'])
+        time.sleep(20.0)
     
